3db3ef2054ac37d098e62a75e0f7db1b4737a853
akarreg <161454874+akarreg@users.noreply.github.com>
2024-11-05 15:53:39 +0100

    making of a Weahter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Weather-App">
+      <header>
+        <div className="Search">
+          <input type="search" placeholder="Enter City"></input>
+          <button> Search </button>
+        </div>
       </header>
+      <main>
+        <h1> City name</h1>
+        <h3>Date</h3>
+
+        <ul>
+          <li>Humidity:</li>
+          <li> Wind : </li>
+        </ul>
+        <Container>
+          <Row>
+            <Col>Tuesday</Col>
+            <Col>Wednesday</Col>
+            <Col>Thursday</Col>
+            <Col>Friday</Col>
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <p>
+          This project was coded by Ama Akarregi and is open-sourced on GitHub
+          and hosted on Netlify
+        </p>
+      </footer>
     </div>
   );
 }
-
-export default App;
