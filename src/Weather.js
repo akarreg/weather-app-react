a0a02de38cb7e7bd67470809e3e97aa896580aec
akarreg <161454874+akarreg@users.noreply.github.com>
2024-11-11 14:14:47 +0100

    start with css and flex need to add days

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,31 @@
 import React from "react";
 import "./Weather.css";
-import "bootstrap/dist/css/bootstrap.css";
 
 export default function Weather() {
   return (
-    <div className="Container">
+    <div className="Weather">
       <header>
         <form>
-          <input type="search" placeholder="Enter City....."></input>
-          <button className="btn-primary"> Search </button>
+          <input type="search" placeholder="   Enter City....."></input>
+
+          <button> Search </button>
         </form>
       </header>
       <main>
-        <h1> City name</h1>
-        <h3>Date </h3>
-
-        <ul>
-          <li>Humidity:</li>
-          <li> Wind : </li>
-          <li> Visibility: </li>
-        </ul>
+        <div className="MainName">
+          <h1> City name</h1>
+          <h3>Date </h3>
+        </div>
+        <div className="row ">
+          <div className="col-6 d-flex justify-content-evenly">🌨️ 6 c</div>
+          <div className="col-6 d-flex justify-content-evenly">
+            <ul>
+              <li>Humidity: 100%</li>
+              <li> Wind : </li>
+              <li> Visibility: </li>
+            </ul>
+          </div>
+        </div>
       </main>
     </div>
   );
